cf4291075a974c9d593e9333eeb578352fba03bf
ShermanWu <shermanwu89@gmail.com>
2015-02-12 22:14:00 +0800

    Added transmit and receive interval timers

--- Hydroponics_Hub/Hydroponics_Hub.ino
+++ Hydroponics_Hub/Hydroponics_Hub.ino
@@ -5,36 +5,50 @@
 #include <MirfSpiDriver.h>
 
 #include "FastLED.h"
-
+//Set number of WS2812B LEDs
 #define NUM_LEDS 1
+//Set data pin for WS2812B LEDs
 #define DATA_PIN 8
+//Create LED object
 CRGB leds[NUM_LEDS];
+//Set initial LED colour
 char led_colour = 'B';
-
+unsigned long last_transmit_millis = 0;
+//Set transmit interval in seconds
+unsigned long transmit_interval = 60;
 void setup()
 {
-  Serial.begin(9600);
+  transmit_interval = (transmit_interval + 10) * 1000;
+  //Add LED config
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  //Set Mirf (NRF24L01+) pins
   Mirf.csnPin = 10;
   Mirf.cePin = 9;
   Mirf.spi = &MirfHardwareSpi;
   Mirf.init();
-
+  //Set Mirf receive address
   Mirf.setRADDR((byte *) "hydrohub");
+  //Set payload size of NRF24L01+ in bytes
   Mirf.payload = 32;
-
+  //Commit Mirf settings
   Mirf.config();
-
- 
 }
 
 void loop()
 {
+  //Check if transmit interval has elapsed without any transmissions
+  if((millis() - last_transmit_millis) > transmit_interval){
+    led_colour = 'B';
+  }
+  Serial.println(millis() - last_transmit_millis);
+  
+  //If data is received
   if (Mirf.dataReady()) {
+    last_transmit_millis = millis();
     byte data[32];
     Mirf.getData((byte *) &data);
     String id = String((char *)data);
-    Serial.println();
+    Serial.println(data[11]);
     if(data[11] == 1){
       led_colour = 'R';
     }else{
@@ -44,6 +58,7 @@ void loop()
   led_show(led_colour);
 }
 
+//Function for changing LED colour
 void led_show(char colour){
   switch(colour){
   case 'R':
@@ -59,3 +74,4 @@ void led_show(char colour){
   
   FastLED.show();
 }
+
--- Hydroponics_Water_Level_Sensor/Hydroponics_Water_Level_Sensor.ino
+++ Hydroponics_Water_Level_Sensor/Hydroponics_Water_Level_Sensor.ino
@@ -2,7 +2,6 @@
 #include <Mirf.h>
 #include <nRF24L01.h>
 #include <MirfHardwareSpiDriver.h>
-#include <MirfSpiDriver.h>
 
 #include <TBClient.h>
 #include <TBWrapper.h>
@@ -12,29 +11,32 @@ const String systemID = "8a6974ff";
 TBClient client((byte *) "hyd01", 32);
 
 byte water_level_sensor_status = 0;
-
+//Set transmit interval in seconds
+unsigned long transmit_interval = 60;
 
 void clientSetup()
 {
+  transmit_interval = transmit_interval * 1000;
   pinMode(5, INPUT_PULLUP);
 }
 
 void clientLoop()
 {
   byte water_level_sensor_current = digitalRead(5);
-
+  /*
   if (water_level_sensor_status != water_level_sensor_current) {
     water_level_sensor_status = water_level_sensor_current;
     sendDataWithIDAndStatus(systemID, water_level_sensor_status);
-  }
-
+  }*/
+  sendDataWithIDAndStatus(systemID, water_level_sensor_current);
+  delay(transmit_interval);
 }
 
 void sendDataWithIDAndStatus(String id, byte status)
 {
   byte sensorStatus[12];
   id.getBytes(sensorStatus, 11);
-  doorStatus[11] = status;
+  sensorStatus[11] = status;
   client.sendData((byte *)"hydrohub", (byte *)sensorStatus);
   free(sensorStatus);
 }
