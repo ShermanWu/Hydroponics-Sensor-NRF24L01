a6dfa86faa13166e0ea110dd63f7a1dbe241fdad
ShermanWu <shermanwu89@gmail.com>
2015-02-28 14:58:31 +0800

    Commented Hydroponics_Hub.ino

--- Hydroponics_Hub/Hydroponics_Hub.ino
+++ Hydroponics_Hub/Hydroponics_Hub.ino
@@ -5,22 +5,38 @@
 #include <MirfSpiDriver.h>
 
 #include "FastLED.h"
+
 //Set number of WS2812B LEDs
 #define NUM_LEDS 1
+
 //Set data pin for WS2812B LEDs
 #define DATA_PIN 8
+
 //Create LED object
 CRGB leds[NUM_LEDS];
+
 //Set initial LED colour
 char led_colour = 'B';
+
 unsigned long last_transmit_millis = 0;
+int failed_trans = 0;
+
 //Set transmit interval in seconds
 unsigned long transmit_interval = 60;
+
+//Failed tranmissions limit (for blue disconnect led)
+int failed_trans_limit = 5;
+
+
 void setup()
 {
-  transmit_interval = transmit_interval * 1000 * 1.3; // add extra time to the interval to account for differences in low power mode
+  Serial.begin(9600);
+  // add extra time to the interval to account for differences in low power mode
+  transmit_interval = transmit_interval * 1000 * 1.3; 
+  
   //Add LED config
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  
   //Set Mirf (NRF24L01+) pins
   Mirf.csnPin = 10;
   Mirf.cePin = 9;
@@ -38,17 +54,26 @@ void loop()
 {
   //Check if transmit interval has elapsed without any transmissions
   if((millis() - last_transmit_millis) > transmit_interval){
-    led_colour = 'B';
+    failed_trans = failed_trans + 1;
+    last_transmit_millis = millis();
+    if(failed_trans >= failed_trans_limit){
+      led_colour = 'B';
+    }
+    Serial.println(failed_trans);
   }
-  Serial.println(millis() - last_transmit_millis);
+  //Serial.println(millis() - last_transmit_millis);
   
   //If data is received
   if (Mirf.dataReady()) {
+    //Log last tranmission time
     last_transmit_millis = millis();
+    //Reset failed transmission counter
+    failed_trans = 0;
+    
     byte data[32];
     Mirf.getData((byte *) &data);
     String id = String((char *)data);
-    Serial.println(data[11]);
+    
     if(data[11] == 1){
       led_colour = 'R';
     }else{
@@ -57,6 +82,8 @@ void loop()
   }
   led_show(led_colour);
 }
+
+
 
 //Function for changing LED colour
 void led_show(char colour){
